467fca566d2a58c9cdfb7357de941cd5ac96bfce
Han Ji Yun <yunhit@sookmyung.ac.kr>
2023-04-18 06:19:02 +0900

    프론트 개발 10.React로 목록 추가, 수정, 삭제하기

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
       ], 
     }; 
   }
-  // (1) add 함수 추가
+  // add 함수 추가
   add =(item)=>{ 
     const thisItems =this.state.items; 
     item.id ="ID-"+ thisItems.length;//key를 위한 id 추가
@@ -24,6 +24,16 @@ class App extends React.Component {
     console.log("items:",this.state.items);
 
   } 
+  // delete 함수 추가. (alt+shift+f 포맷팅)
+  delete =(item)=>{ 
+    const thisItems =this.state.items; 
+    const newItems = thisItems.filter(e => e.id !== item.id); 
+    this.setState({items:newItems},()=>{ 
+      //디버깅 콜백
+      console.log("Update Items : ",this.state.items)
+    });
+  }
+
   render(){ 
     // todoItems에 this.state.items.length 가 0보다 크다면 true 이므로 && 뒤에 값을 넘겨준다. 
     // totoItem = this.state.items.length > 0 ? (<Paper></Paper>):""; 이렇게 해도 같은 결과이다. 조건선택문 ? ternary operator
@@ -31,14 +41,14 @@ class App extends React.Component {
       <Paper style={{margin:16}}>
         <List>
         {this.state.items.map((item,idx)=>(
-        <Todo item={item} key={item.id}/>
+        <Todo item={item} key={item.id} delete={this.delete}/>
         ))} 
         </List>
       </Paper>
     
     );
     
-    // (2) add 함수 연결
+    // 생선된 컴포넌트 JSX를 리턴한다.
     return (
       <div className="App">
         <Container maxWidth="md">
--- src/Todo.js
+++ src/Todo.js
@@ -1,30 +1,70 @@
 import React from 'react';
-import {ListItem, ListItemText, InputBase, Checkbox}from "@material-ui/core";
+import {ListItem, ListItemText, InputBase, Checkbox, ListItemSecondaryAction, IconButton}from"@material-ui/core";
+import DeleteOutlined from "@material-ui/icons/DeleteOutlined";
 
-class Todo extends React.Component {
-    constructor(props){
+class Todo extends React.Component { 
+    constructor(props){ 
         super(props); 
-        this.state ={ item: props.item }; // 매개변수 item 의 변수/값을 item에 대입
+        this.state ={ item: props.item, readOnly:true }; // 매개변수 item 의 변수/값을 item에 대입
+        this.delete = props.delete; 
     }
-
+ 
+    deleteEventHandler =()=>{ 
+        this.delete(this.state.item); 
+    } 
+    offReadOnlyMode =()=>{ 
+        console.log("Event!",this.state.readOnly); 
+        this.setState({readOnly:false},()=>{ 
+            console.log("ReadOnly?",this.state.readOnly)
+        }); 
+    } 
+    enterKeyEventHandler =(e)=>{ 
+        if(e.key ==="Enter"){ 
+            this.setState({readOnly:true})
+        } 
+    } 
+    editEventHandler =(e)=>{ 
+        const thisItem =this.state.item; 
+        thisItem.title = e.target.value;
+        this.setState({item:thisItem}); 
+    } 
+    checkboxEventHander =(e)=>{ 
+        console.log("check box event call"); 
+        const thisItem =this.state.item; 
+        thisItem.done = thisItem.done ?false :true;// thisItemdone = !thisitem.done
+        this.setState({item:thisItem}); 
+    } 
     render(){ 
         const item =this.state.item; 
         return (
             <ListItem>
-                <Checkbox checked={item.done}/>
+                <Checkbox
+                checked={item.done}
+                onChange={this.checkboxEventHander} 
+                />
                 <ListItemText>
                     <InputBase
-                    inputProps={{"aria-label":"naked"}}
+                    inputProps={{"aria-label":"naked", readOnly:this.state.readOnly}} 
                     type="text"
                     id={item.id} 
                     name={item.id} 
                     value={item.title} 
                     multiline={true} 
                     fullWidth={true} 
+                    onClick={this.offReadOnlyMode} 
+                    onChange={this.editEventHandler} 
+                    onKeyPress={this.enterKeyEventHandler}
                     />
                 </ListItemText>
+ 
+                <ListItemSecondaryAction>
+                    <IconButton aria-label="Delete"
+                    onClick={this.deleteEventHandler}>
+                        <DeleteOutlined />
+                    </IconButton>
+                </ListItemSecondaryAction>
             </ListItem>
-        ); 
+     ); 
     }
 }
 
