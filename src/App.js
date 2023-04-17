15a802e9781835f19e19d42695bcb50010de9f56
Han Ji Yun <yunhit@sookmyung.ac.kr>
2023-04-18 05:50:03 +0900

    프론트 개발 8-2. React에서 리스트 렌더링하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Todo from './Todo';
+import {Paper, List}from "@material-ui/core";
 import './App.css';
 
 class App extends React.Component {
@@ -7,18 +8,27 @@ class App extends React.Component {
     super(props); // 매개변수 pros 초기화
     this.state ={ // item 에 item.id, item.title, item.done 매개변수 이름과 값 할당
       items :[ 
-        {id:0, title:"Todo 1 ", done:false}, 
-        {id:1, title:"Todo 2 ", done:false}, 
-      ], 
-    }; 
-  } 
-  render(){ // 자바스크립트가 제공하는 map 함수를 이용해서 배열을 반복해 <Todo /> 컴포넌트를 여러개 생성한다. 
-    var todoItems =this.state.items.map((item,idx)=>(
-      <Todo item={item} key={item.id}/>
-    ));
+        {id:0, title:"Todo 1 ", done:true},
+        {id:1, title:"Todo 2 ", done:false},
+       ], 
+      }; 
+    } 
+    render(){ 
+      // todoItems에 this.state.items.length 가 0보다 크다면 true 이므로 && 뒤에 값을 넘겨준다. 
+      // totoItem = this.state.items.length > 0 ? (<Paper></Paper>):""; 이렇게 해도 같은 결과이다. 조건선택문 ? ternary operator
+      var todoItems =this.state.items.length >0 &&(
+        <Paper style={{margin:16}}>
+          <List>
+            {this.state.items.map((item,idx)=>(
+            <Todo item={item} key={item.id}/>
+            ))} 
+            </List>
+        </Paper>
+      );
 
-    // 생성된 컴포넌트 JSX를 리턴한다. 
-    return <div className="App">{todoItems}</div>; 
-  }
+      // 생성된 컴포넌트 JSX를 리턴한다. 
+      return <div className="App">{todoItems}</div>; 
+    }
 }
+
 export default App;
