7be72b2d6f76916d6355e95d709334583b9ae46d
Han Ji Yun <yunhit@sookmyung.ac.kr>
2023-04-18 05:21:26 +0900

    프론트 개발 7.Props, state 사용하기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import Hello from './Hello';
+import Todo from './Todo';
 import './App.css';
 
-class App extends React.Component {
-   render(){ 
-    return (
+class App extends React.Component { 
+  constructor(props){ //매개변수 props 생성자
+    super(props); // 매개변수 pros 초기화
+    this.state ={ // item 에 item.id, item.title, item.done 매개변수 이름과 값 할당
+      item :{id:0, title:"Hello World 1 ", done:true},
+     }; 
+  }
+  render(){ 
+    return ( // 매개변수 item 에 변수명/값을 전달
       <div className="App">
-        <Hello />
+        <Todo item={this.state.item}/>
       </div>
-   ); 
- }
+    );
+   }
 }
-
 export default App;
---
+++ src/Todo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Todo extends React.Component { 
+    constructor(props){
+         super(props); 
+         this.state ={ item: props.item }; // 매개변수 item 의 변수/값을 item에 대입
+    } 
+    render(){ 
+        return (
+            <div className="Todo">
+                <input
+                    type="checkbox"
+                    id={this.state.item.id} // item.id 값으로 렌더링하란 의미(JPX)
+                    name={this.state.item.id} checked={this.state.item.done} // item.done 값으로 렌더링하란 의미
+                />
+                <label for={this.state.item.id}>{this.state.item.title}</label>
+            
+            </div>
+        ); 
+    }
+}
+export default Todo;
