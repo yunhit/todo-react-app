0b766598579f08ad94be222e0f359361e4b5b9ea
Han Ji Yun <yunhit@sookmyung.ac.kr>
2023-04-18 05:35:07 +0900

    프론트 개발 8-1. React에서 리스트 렌더링하기

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import Todo from './Todo';
 import './App.css';
 
-class App extends React.Component { 
-  constructor(props){ //매개변수 props 생성자
+class App extends React.Component {
+   constructor(props){ //매개변수 props 생성자
     super(props); // 매개변수 pros 초기화
     this.state ={ // item 에 item.id, item.title, item.done 매개변수 이름과 값 할당
-      item :{id:0, title:"Hello World 1 ", done:true},
-     }; 
+      items :[ 
+        {id:0, title:"Todo 1 ", done:false}, 
+        {id:1, title:"Todo 2 ", done:false}, 
+      ], 
+    }; 
+  } 
+  render(){ // 자바스크립트가 제공하는 map 함수를 이용해서 배열을 반복해 <Todo /> 컴포넌트를 여러개 생성한다. 
+    var todoItems =this.state.items.map((item,idx)=>(
+      <Todo item={item} key={item.id}/>
+    ));
+
+    // 생성된 컴포넌트 JSX를 리턴한다. 
+    return <div className="App">{todoItems}</div>; 
   }
-  render(){ 
-    return ( // 매개변수 item 에 변수명/값을 전달
-      <div className="App">
-        <Todo item={this.state.item}/>
-      </div>
-    );
-   }
 }
 export default App;
